91c034fd1aacdb3e658f98605d8a8a26637b4acd
WestBrasilBI <rafael.leonardo@westbrasil.com.br>
2026-09-02 14:40:08 -0300

    deploy: sw.js (2026-09-02 14:39)

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'consulta-precos-v1';
+const ASSETS = ['./', './index.html', './data/data.js', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    // online: sempre busca a versão mais nova; offline: usa a última salva
+    fetch(e.request, { cache: 'no-store' })
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
